39d086aa96d17a7a533789744e34468e5caa85a5
Robert <robert.schlegel@imev-mer.fr>
2024-04-25 09:27:15 +0200

    First pass at BO v3.0 tests

--- tests/test_BO.R
+++ tests/test_BO.R
@@ -181,38 +181,17 @@ plot_sws_depthsurf_2 <- ggplot(data = curvel_ss_test, aes(x = lon, y = lat)) +
   labs(fill = "Max greater than min", x = NULL, y = NULL,
        title = "Surface currents v3.0; rast_2010: max vs min") +
   theme(legend.position = "bottom")
-ggsave(plot = plot_sws_depthsurf_2, filename = "tests/sws_depthsurf_ltmax_2.png", height = 5, width = 8)
+ggsave(plot = plot_sws_depthsurf_2, filename = "tests/sws_depthsurf_rast_2010.png", height = 5, width = 8)
 
-# Same test but for csv format download
-# NB: Slow, but does run. Takes a few minutes.
-current_surf_csv <- download_layers(dataset_id = "sws_baseline_2000_2019_depthsurf", 
-                                     variables = c("sws_ltmax", "sws_ltmin"), 
-                                     constraints = set_con, fmt = "csv", directory = dir)
-current_surf_csv_test <- current_surf_csv |> na.omit() |> 
-  dplyr::rename(lon = longitude, lat = latitude) |> 
-  mutate(lon = round(lon, 4), lat = round(lat, 4)) |>
-  mutate(max_min = ifelse(sws_ltmax > sws_ltmin, TRUE, FALSE))
-plot_sws_surf_ltmax_csv <- ggplot(data = current_surf_csv_test, aes(x = lon, y = lat)) +
-  geom_raster(aes(fill = max_min)) + coord_quickmap(expand = F) +
-  labs(fill = "Max greater than min", x = NULL, y = NULL,
-       title = "BottomSurface currents v3.0; sws_ltmax_csv vs sws_ltmin_csv") +
+# Compare rast_2000 to rast_2010
+plot_sws_depthsurf_2000_v_2010 <- curvel_ss_test |> 
+  mutate(max_min_rast_2000_v_max_min_rast_2010 = if_else(max_min_rast_2000 == max_min_rast_2010, TRUE, FALSE)) |> 
+  ggplot(aes(x = lon, y = lat)) +
+  geom_raster(aes(fill = max_min_rast_2000_v_max_min_rast_2010)) + coord_quickmap(expand = F) +
+  labs(fill = "Mismatches are the same", x = NULL, y = NULL,
+       title = "Surface currents v3.0; rast_2000 vs rast_2010") +
   theme(legend.position = "bottom")
-ggsave(plot = plot_sws_surf_ltmax_csv, filename = "tests/sws_surf_ltmax_csv.png", height = 5, width = 8)
-
-# Same test but for NetCDF format download
-current_bdmax_nc <- download_layers(dataset_id = "sws_baseline_2000_2019_depthmax", 
-                                    variables = c("sws_ltmax", "sws_ltmin"), 
-                                    constraints = set_con, fmt = "nc", directory = dir)
-current_bdmax_nc_test <- current_bdmax_nc$data |> na.omit() |> 
-  dplyr::rename(lon = longitude, lat = latitude) |> 
-  mutate(lon = round(lon, 4), lat = round(lat, 4)) |>
-  mutate(max_min = ifelse(sws_ltmax > sws_ltmin, TRUE, FALSE))
-plot_sws_depthmax_ltmax_nc <- ggplot(data = current_bdmax_nc_test, aes(x = lon, y = lat)) +
-  geom_raster(aes(fill = max_min)) + coord_quickmap(expand = F) +
-  labs(fill = "Max greater than min", x = NULL, y = NULL,
-       title = "Bottom currents v3.0; sws_ltmax_nc vs sws_ltmin_nc") +
-  theme(legend.position = "bottom")
-ggsave(plot = plot_sws_depthmax_ltmax_nc, filename = "tests/sws_depthmax_ltmax_nc.png", height = 5, width = 8)
+ggsave(plot = plot_sws_depthsurf_2000_v_2010, filename = "tests/sws_depthsurf_rast_2000_v_rast_2010.png", height = 5, width = 8)
 
 
 # Test SST layer ----------------------------------------------------------
@@ -227,22 +206,33 @@ thetao_surface_layers <- download_layers(dataset_id = "thetao_baseline_2000_2019
 thetao_ss_test <- as.data.frame(thetao_surface_layers, xy = T) |> 
   dplyr::rename(lon = x, lat = y) |> na.omit() |> 
   mutate(lon = round(lon, 4), lat = round(lat, 4)) |> 
-  mutate(max_min_1 = ifelse(thetao_ltmax_1 > thetao_ltmin_1, TRUE, FALSE),
-         max_min_2 = ifelse(thetao_ltmax_2 > thetao_ltmin_2, TRUE, FALSE))
+  mutate(max_min_rast_2000 = ifelse(thetao_ltmax_1 > thetao_ltmin_1, TRUE, FALSE),
+         max_min_rast_2010 = ifelse(thetao_ltmax_2 > thetao_ltmin_2, TRUE, FALSE)) |> 
+  dplyr::select(-thetao_ltmax_1, -thetao_ltmax_2, -thetao_ltmin_1, -thetao_ltmin_2)
 
 # Visualise pixels where the max and min values are not as expected
-plot_thetao_depthsurf_1 <- ggplot(data = thetao_ss_test, aes(x = lon, y = lat)) +
-  geom_raster(aes(fill = max_min_1)) + coord_quickmap(expand = F) +
+plot_thetao_depthsurf_2000 <- ggplot(data = thetao_ss_test, aes(x = lon, y = lat)) +
+  geom_raster(aes(fill = max_min_rast_2000)) + coord_quickmap(expand = F) +
   labs(fill = "Max greater than min", x = NULL, y = NULL,
-       title = "Surface temperature v3.0; thetao_ltmax_1 vs thetao_ltmin_2") +
+       title = "Surface temperature v3.0; thetao_2000: max vs min") +
   theme(legend.position = "bottom")
-ggsave(plot = plot_thetao_depthsurf_1, filename = "tests/thetao_depthsurf_ltmax_1.png", height = 5, width = 8)
+ggsave(plot = plot_thetao_depthsurf_2000, filename = "tests/thetao_depthsurf_rast_2000.png", height = 5, width = 8)
 
 # Visualise pixels where the max and min values are not as expected
-plot_thetao_depthsurf_2 <- ggplot(data = curvel_ss_test, aes(x = lon, y = lat)) +
-  geom_raster(aes(fill = max_min_2)) + coord_quickmap(expand = F) +
+plot_thetao_depthsurf_2010 <- ggplot(data = thetao_ss_test, aes(x = lon, y = lat)) +
+  geom_raster(aes(fill = max_min_rast_2010)) + coord_quickmap(expand = F) +
   labs(fill = "Max greater than min", x = NULL, y = NULL,
-       title = "Surface temperature v3.0; thetao_ltmax_2 vs thetao_ltmin_2") +
+       title = "Surface temperature v3.0; thetao_2010: max vs min") +
   theme(legend.position = "bottom")
-ggsave(plot = plot_thetao_depthsurf_2, filename = "tests/thetao_depthsurf_ltmax_2.png", height = 5, width = 8)
+ggsave(plot = plot_thetao_depthsurf_2010, filename = "tests/thetao_depthsurf_rast_2010.png", height = 5, width = 8)
+
+# Compare rast_2000 to rast_2010
+plot_thetao_depthsurf_2000_v_2010 <- thetao_ss_test |> 
+  mutate(max_min_rast_2000_v_max_min_rast_2010 = if_else(max_min_rast_2000 == max_min_rast_2010, TRUE, FALSE)) |> 
+  ggplot(aes(x = lon, y = lat)) +
+  geom_raster(aes(fill = max_min_rast_2000_v_max_min_rast_2010)) + coord_quickmap(expand = F) +
+  labs(fill = "Mismatches are the same", x = NULL, y = NULL,
+       title = "Surface temperature v3.0; rast_2000 vs rast_2010") +
+  theme(legend.position = "bottom")
+ggsave(plot = plot_thetao_depthsurf_2000_v_2010, filename = "tests/thetao_depthsurf_rast_2000_v_rast_2010.png", height = 5, width = 8)
 
