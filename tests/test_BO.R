662cd0d34f63a45aa660751cf91fae71b8118ae2
Robert <robert.schlegel@imev-mer.fr>
2024-06-11 08:27:47 +0200

    More tests for BO v3.0

--- tests/test_BO.R
+++ tests/test_BO.R
@@ -1,7 +1,10 @@
 # tests/test_BO.R
-# The purpose of this script is to test the Bio-Oracle data
+# The purpose of this script is to test the v3.0 Bio-Oracle data
 # More tests available here:
 # https://github.com/robwschlegel/ArcticKelp/blob/master/tests/test_BO.R
+
+
+# Setup -------------------------------------------------------------------
 
 # Load tidyverse
 library(tidyverse)
@@ -14,225 +17,103 @@ library(biooracler) # New version
 library(raster)
 
 # Set cores
+library(doParallel)
 registerDoParallel(cores = 15)
 
 # Disable scientific notation
 options(scipen = 999)
 
 # Check layers
-BO_layers <- biooracler::list_layers(simplify = FALSE)
-
-# Set global constraints in advance
-time_con <-  c("2001-01-01T00:00:00Z", "2010-01-01T00:00:00Z")
-lat_con <- c(-89.975, 89.975)
-lon_con <- c(-179.975, 179.975)
-set_con <- list(time_con, lat_con, lon_con)
-names(set_con) = c("time", "latitude", "longitude")
+BO_layers <- biooracler::list_layers(simplify = TRUE)
 
 # Set temporary directory for testing
 dir <- tempdir()
 
-
-# Tests of bottom current layers ------------------------------------------
-
-# Info
-info_layer("sws_baseline_2000_2019_depthmax")
-
-# Test bottom currents
-current_bdmax_layers <- download_layers(dataset_id = "sws_baseline_2000_2019_depthmax", 
-                                        variables = c("sws_ltmax", "sws_ltmin"), 
-                                        constraints = set_con)
-current_bdmax_test <- as.data.frame(current_bdmax_layers, xy = T) |> 
-  dplyr::rename(lon = x, lat = y) |> na.omit() |> 
-  mutate(lon = round(lon, 4), lat = round(lat, 4)) |>
-  mutate(max_min_rast_2000 = ifelse(sws_ltmax_1 > sws_ltmin_1, TRUE, FALSE),
-         max_min_rast_2010 = ifelse(sws_ltmax_2 > sws_ltmin_2, TRUE, FALSE)) |> 
-  dplyr::select(-sws_ltmax_1, -sws_ltmax_2, -sws_ltmin_1, -sws_ltmin_2)
-
-# Visualise pixels where the max and min values are not as expected
-plot_sws_depthmax_2000 <- ggplot(data = current_bdmax_test, aes(x = lon, y = lat)) +
-  geom_raster(aes(fill = max_min_rast_2000)) + coord_quickmap(expand = F) +
-  labs(fill = "Max greater than min", x = NULL, y = NULL,
-       title = "Bottom currents v3.0; rast_2000: max vs min") +
-  theme(legend.position = "bottom")
-ggsave(plot = plot_sws_depthmax_2000, filename = "tests/sws_depthmax_rast_2000.png", height = 5, width = 8)
-plot_sws_depthmax_2010 <- ggplot(data = current_bdmax_test, aes(x = lon, y = lat)) +
-  geom_raster(aes(fill = max_min_rast_2010)) + coord_quickmap(expand = F) +
-  labs(fill = "Max greater than min", x = NULL, y = NULL,
-       title = "Bottom currents v3.0; rast_2010: max vs min") +
-  theme(legend.position = "bottom")
-ggsave(plot = plot_sws_depthmax_2010, filename = "tests/sws_depthmax_rast_2010.png", height = 5, width = 8)
-
-# Same test but for csv format download
-# NB: Slow, but does run. Takes a few minutes.
-current_bdmax_csv <- download_layers(dataset_id = "sws_baseline_2000_2019_depthmax", 
-                                     variables = c("sws_ltmax", "sws_ltmin"), 
-                                     constraints = set_con, fmt = "csv", directory = dir)
-current_bdmax_csv_test <- current_bdmax_csv |> na.omit() |> 
-  dplyr::rename(lon = longitude, lat = latitude) |> 
-  mutate(lon = round(lon, 4), lat = round(lat, 4)) |>
-  mutate(max_min_csv = ifelse(sws_ltmax > sws_ltmin, TRUE, FALSE)) |> 
-  mutate(variable = case_when(time == "2000-01-01T00:00:00Z" ~ "max_min_csv_2000",
-                              time == "2010-01-01T00:00:00Z" ~ "max_min_csv_2010", 
-                              TRUE ~ as.character(NA))) |> 
-  dplyr::select(-time, -sws_ltmax, -sws_ltmin) |> 
-  pivot_wider(names_from = variable, values_from = max_min_csv)
-plot_sws_depthmax_csv_2000 <- ggplot(data = current_bdmax_csv_test, aes(x = lon, y = lat)) +
-  geom_raster(aes(fill = max_min_csv_2000)) + coord_quickmap(expand = F) +
-  labs(fill = "Max greater than min", x = NULL, y = NULL,
-       title = "Bottom currents v3.0; csv_2000: max vs min") +
-  theme(legend.position = "bottom")
-ggsave(plot = plot_sws_depthmax_csv_2000, filename = "tests/sws_depthmax_csv_2000.png", height = 5, width = 8)
-plot_sws_depthmax_csv_2010 <- ggplot(data = current_bdmax_csv_test, aes(x = lon, y = lat)) +
-  geom_raster(aes(fill = max_min_csv_2010)) + coord_quickmap(expand = F) +
-  labs(fill = "Max greater than min", x = NULL, y = NULL,
-       title = "Bottom currents v3.0; csv_2010: max vs min") +
-  theme(legend.position = "bottom")
-ggsave(plot = plot_sws_depthmax_csv_2010, filename = "tests/sws_depthmax_csv_2010.png", height = 5, width = 8)
-
-# Same test but for NetCDF format download
-current_bdmax_nc <- download_layers(dataset_id = "sws_baseline_2000_2019_depthmax", 
-                                     variables = c("sws_ltmax", "sws_ltmin"), 
-                                     constraints = set_con, fmt = "nc", directory = dir)
-current_bdmax_nc_test <- current_bdmax_nc$data |> na.omit() |> 
-  dplyr::rename(lon = longitude, lat = latitude) |> 
-  mutate(lon = round(lon, 4), lat = round(lat, 4)) |>
-  mutate(max_min_nc = ifelse(sws_ltmax > sws_ltmin, TRUE, FALSE)) |> 
-  mutate(variable = case_when(time == "2000-01-01T00:00:00Z" ~ "max_min_nc_2000",
-                              time == "2010-01-01T00:00:00Z" ~ "max_min_nc_2010", 
-                              TRUE ~ as.character(NA))) |> 
-  dplyr::select(-time, -sws_ltmax, -sws_ltmin) |> 
-  pivot_wider(names_from = variable, values_from = max_min_nc)
-plot_sws_depthmax_nc_2000 <- ggplot(data = current_bdmax_nc_test, aes(x = lon, y = lat)) +
-  geom_raster(aes(fill = max_min_nc_2000)) + coord_quickmap(expand = F) +
-  labs(fill = "Max greater than min", x = NULL, y = NULL,
-       title = "Bottom currents v3.0; nc_2000: max vs min") +
-  theme(legend.position = "bottom")
-ggsave(plot = plot_sws_depthmax_nc_2000, filename = "tests/sws_depthmax_nc_2000.png", height = 5, width = 8)
-plot_sws_depthmax_nc_2010 <- ggplot(data = current_bdmax_nc_test, aes(x = lon, y = lat)) +
-  geom_raster(aes(fill = max_min_nc_2010)) + coord_quickmap(expand = F) +
-  labs(fill = "Max greater than min", x = NULL, y = NULL,
-       title = "Bottom currents v3.0; nc_2010: max vs min") +
-  theme(legend.position = "bottom")
-ggsave(plot = plot_sws_depthmax_nc_2010, filename = "tests/sws_depthmax_nc_2010.png", height = 5, width = 8)
-
-# Join all tests for further plotting
-current_bdmax_all_test <- left_join(current_bdmax_test, current_bdmax_csv_test) |> 
-  left_join(current_bdmax_nc_test)
-
-# Compare rast_2000 to rast_2010
-plot_sws_depthmax_rast_2000_v_rast_2010 <- current_bdmax_all_test |> 
-  mutate(max_min_rast_2000_v_max_min_rast_2010 = if_else(max_min_rast_2000 == max_min_rast_2010, TRUE, FALSE)) |> 
-  ggplot(aes(x = lon, y = lat)) +
-  geom_raster(aes(fill = max_min_rast_2000_v_max_min_rast_2010)) + coord_quickmap(expand = F) +
-  labs(fill = "Mismatches are the same", x = NULL, y = NULL,
-       title = "Bottom currents v3.0; rast_2000 vs rast_2010") +
-  theme(legend.position = "bottom")
-ggsave(plot = plot_sws_depthmax_rast_2000_v_rast_2010, filename = "tests/sws_depthmax_rast_2000_v_rast_2010.png", height = 5, width = 8)
-
-# Compare rast_2000 to csv_2000
-plot_sws_depthmax_rast_2000_v_csv_2000 <- current_bdmax_all_test |> 
-  mutate(max_min_rast_2000_v_max_min_csv_2000 = if_else(max_min_rast_2000 == max_min_csv_2000, TRUE, FALSE)) |> 
-  ggplot(aes(x = lon, y = lat)) +
-  geom_raster(aes(fill = max_min_rast_2000_v_max_min_csv_2000)) + coord_quickmap(expand = F) +
-  labs(fill = "Mismatches are the same", x = NULL, y = NULL,
-       title = "Bottom currents v3.0; rast_2000 vs csv_2000") +
-  theme(legend.position = "bottom")
-ggsave(plot = plot_sws_depthmax_rast_2000_v_csv_2000, filename = "tests/sws_depthmax_rast_2000_v_csv_2000.png", height = 5, width = 8)
-
-# Compare rast_2000 to csv_2000
-plot_sws_depthmax_rast_2010_v_nc_2010 <- current_bdmax_all_test |> 
-  mutate(max_min_rast_2010_v_max_min_nc_2010 = if_else(max_min_rast_2010 == max_min_nc_2010, TRUE, FALSE)) |> 
-  ggplot(aes(x = lon, y = lat)) +
-  geom_raster(aes(fill = max_min_rast_2010_v_max_min_nc_2010)) + coord_quickmap(expand = F) +
-  labs(fill = "Mismatches are the same", x = NULL, y = NULL,
-       title = "Bottom currents v3.0; rast_2010 vs nc_2010") +
-  theme(legend.position = "bottom")
-ggsave(plot = plot_sws_depthmax_rast_2010_v_nc_2010, filename = "tests/sws_depthmax_rast_2010_v_nc_2010.png", height = 5, width = 8)
-
-
-# Test surface current layer ----------------------------------------------
-
-# Info
-info_layer("sws_baseline_2000_2019_depthsurf")
-
-# Test surface currents
-current_surface_layers <- download_layers(dataset_id = "sws_baseline_2000_2019_depthsurf", 
-                                          variables = c("sws_ltmax", "sws_ltmin"), 
-                                          constraints = set_con)
-curvel_ss_test <- as.data.frame(current_surface_layers, xy = T) |> 
-  dplyr::rename(lon = x, lat = y) |> na.omit() |> 
-  mutate(lon = round(lon, 4), lat = round(lat, 4)) |> 
-  mutate(max_min_rast_2000 = ifelse(sws_ltmax_1 > sws_ltmin_1, TRUE, FALSE),
-         max_min_rast_2010 = ifelse(sws_ltmax_2 > sws_ltmin_2, TRUE, FALSE)) |> 
-  dplyr::select(-sws_ltmax_1, -sws_ltmax_2, -sws_ltmin_1, -sws_ltmin_2)
-
-# Visualise pixels where the max and min values are not as expected
-plot_sws_depthsurf_1 <- ggplot(data = curvel_ss_test, aes(x = lon, y = lat)) +
-  geom_raster(aes(fill = max_min_rast_2000)) + coord_quickmap(expand = F) +
-  labs(fill = "Max greater than min", x = NULL, y = NULL,
-       title = "Surface currents v3.0; rast_2000: max vs min") +
-  theme(legend.position = "bottom")
-ggsave(plot = plot_sws_depthsurf_1, filename = "tests/sws_depthsurf_rast_2000.png", height = 5, width = 8)
-
-# Visualise pixels where the max and min values are not as expected
-plot_sws_depthsurf_2 <- ggplot(data = curvel_ss_test, aes(x = lon, y = lat)) +
-  geom_raster(aes(fill = max_min_rast_2010)) + coord_quickmap(expand = F) +
-  labs(fill = "Max greater than min", x = NULL, y = NULL,
-       title = "Surface currents v3.0; rast_2010: max vs min") +
-  theme(legend.position = "bottom")
-ggsave(plot = plot_sws_depthsurf_2, filename = "tests/sws_depthsurf_rast_2010.png", height = 5, width = 8)
-
-# Compare rast_2000 to rast_2010
-plot_sws_depthsurf_2000_v_2010 <- curvel_ss_test |> 
-  mutate(max_min_rast_2000_v_max_min_rast_2010 = if_else(max_min_rast_2000 == max_min_rast_2010, TRUE, FALSE)) |> 
-  ggplot(aes(x = lon, y = lat)) +
-  geom_raster(aes(fill = max_min_rast_2000_v_max_min_rast_2010)) + coord_quickmap(expand = F) +
-  labs(fill = "Mismatches are the same", x = NULL, y = NULL,
-       title = "Surface currents v3.0; rast_2000 vs rast_2010") +
-  theme(legend.position = "bottom")
-ggsave(plot = plot_sws_depthsurf_2000_v_2010, filename = "tests/sws_depthsurf_rast_2000_v_rast_2010.png", height = 5, width = 8)
-
-
-# Test SST layer ----------------------------------------------------------
-
 # Info
 info_layer("thetao_baseline_2000_2019_depthsurf")
 
-# Test surface currents
-thetao_surface_layers <- download_layers(dataset_id = "thetao_baseline_2000_2019_depthsurf", 
-                                          variables = c("thetao_ltmax", "thetao_ltmin"), 
-                                          constraints = set_con)
-thetao_ss_test <- as.data.frame(thetao_surface_layers, xy = T) |> 
-  dplyr::rename(lon = x, lat = y) |> na.omit() |> 
-  mutate(lon = round(lon, 4), lat = round(lat, 4)) |> 
-  mutate(max_min_rast_2000 = ifelse(thetao_ltmax_1 > thetao_ltmin_1, TRUE, FALSE),
-         max_min_rast_2010 = ifelse(thetao_ltmax_2 > thetao_ltmin_2, TRUE, FALSE)) |> 
-  dplyr::select(-thetao_ltmax_1, -thetao_ltmax_2, -thetao_ltmin_1, -thetao_ltmin_2)
 
-# Visualise pixels where the max and min values are not as expected
-plot_thetao_depthsurf_2000 <- ggplot(data = thetao_ss_test, aes(x = lon, y = lat)) +
-  geom_raster(aes(fill = max_min_rast_2000)) + coord_quickmap(expand = F) +
-  labs(fill = "Max greater than min", x = NULL, y = NULL,
-       title = "Surface temperature v3.0; thetao_2000: max vs min") +
-  theme(legend.position = "bottom")
-ggsave(plot = plot_thetao_depthsurf_2000, filename = "tests/thetao_depthsurf_rast_2000.png", height = 5, width = 8)
+# Functions ---------------------------------------------------------------
 
-# Visualise pixels where the max and min values are not as expected
-plot_thetao_depthsurf_2010 <- ggplot(data = thetao_ss_test, aes(x = lon, y = lat)) +
-  geom_raster(aes(fill = max_min_rast_2010)) + coord_quickmap(expand = F) +
-  labs(fill = "Max greater than min", x = NULL, y = NULL,
-       title = "Surface temperature v3.0; thetao_2010: max vs min") +
-  theme(legend.position = "bottom")
-ggsave(plot = plot_thetao_depthsurf_2010, filename = "tests/thetao_depthsurf_rast_2010.png", height = 5, width = 8)
+# var_choice <- "thetao"; scenario <- "baseline"
+BO_v3_test <- function(var_choice, scenario = "baseline"){
+  
+  # Get info if necessary
+  if(!exists("BO_layers")) BO_layers <- biooracler::list_layers(simplify = TRUE)
+  
+  # Extract dataset id
+  # NB: This more complicated method is used because not all baseline periods have
+  # the same year range. E.g. 2000-2018 OR 2000-2019
+  dataset_choice <- BO_layers |> 
+    filter(grepl(paste0(var_choice,"_"), dataset_id)) |> 
+    filter(grepl(paste0(scenario,"_"), dataset_id)) |> 
+    filter(grepl("depthsurf", dataset_id)) |> 
+    filter(!grepl("kdpar", dataset_id))
+  
+  # Exit if no values
+  if(nrow(dataset_choice) == 0) return()
+  
+  # Set constraints
+  if(scenario == "baseline") {
+    time_con <-  c("2001-01-01T00:00:00Z", "2010-01-01T00:00:00Z")
+  } else {
+    time_con <-  c("2020-01-01T00:00:00Z", "2090-01-01T00:00:00Z")
+  }
+  lat_con <- c(-20, 89.975); lon_con <- c(-90, 90)
+  set_con <- list(time_con, lat_con, lon_con); names(set_con) = c("time", "latitude", "longitude")
+  
+  # Create variable names
+  if(var_choice == "par"){
+    var_names <- c("PAR_mean_ltmax", "PAR_mean_ltmin")
+    var_name <- "PAR_mean"
+  } else {
+    var_names <- c(paste0(var_choice,"_ltmax"), paste0(var_choice,"_ltmin"))
+    var_name <- var_choice
+  }
+  
+  # Test bottom currents
+  choice_layers <- download_layers(dataset_id = dataset_choice$dataset_id,
+                                   variables = var_names, constraints = set_con)
+  choice_df <- as.data.frame(choice_layers, xy = T) |> 
+    rename_at(.vars = vars(starts_with(var_name)),
+              .funs = list(~ sub(paste0(var_name,"_"), "", .))) |> 
+    dplyr::rename(lon = x, lat = y) |> na.omit() |> 
+    mutate(lon = round(lon, 4), lat = round(lat, 4)) |>
+    mutate(max_min_1 = ifelse(ltmax_1 >= ltmin_1, TRUE, FALSE),
+           max_min_2 = ifelse(ltmax_2 >= ltmin_2, TRUE, FALSE)) |> 
+    dplyr::select(-ltmax_1, -ltmax_2, -ltmin_1, -ltmin_2) |> 
+    pivot_longer(max_min_1:max_min_2)
+  
+  # Visualise pixels where the max and min values are not as expected
+  plot_choice <- ggplot(data = choice_df, aes(x = lon, y = lat)) +
+    geom_raster(aes(fill = value)) + coord_quickmap(expand = F) +
+    labs(fill = "Max greater than min", x = NULL, y = NULL,
+         title = paste0("Bio-Oracle v3.0: ", dataset_choice$dataset_id)) +
+    facet_wrap("name") +
+    theme(panel.border = element_rect(fill = NA, colour = "black"),
+          legend.position = "bottom")
+  ggsave(plot = plot_choice, filename = paste0("tests/",dataset_choice$dataset_id,".png"), height = 5, width = 9)
+  # rm(var_choice, scenario, dataset_choice, var_names, var_name, time_con, lat_con, lon_con, set_con, choice_layers, choice_df, plot_choice); gc()
+}
 
-# Compare rast_2000 to rast_2010
-plot_thetao_depthsurf_2000_v_2010 <- thetao_ss_test |> 
-  mutate(max_min_rast_2000_v_max_min_rast_2010 = if_else(max_min_rast_2000 == max_min_rast_2010, TRUE, FALSE)) |> 
-  ggplot(aes(x = lon, y = lat)) +
-  geom_raster(aes(fill = max_min_rast_2000_v_max_min_rast_2010)) + coord_quickmap(expand = F) +
-  labs(fill = "Mismatches are the same", x = NULL, y = NULL,
-       title = "Surface temperature v3.0; rast_2000 vs rast_2010") +
-  theme(legend.position = "bottom")
-ggsave(plot = plot_thetao_depthsurf_2000_v_2010, filename = "tests/thetao_depthsurf_rast_2000_v_rast_2010.png", height = 5, width = 8)
+
+# Test key variables ------------------------------------------------------
+
+# NB: In an earlier version of this script (see history via GitHub) all of the different data
+# types were also tested, e.g. raster, NetCDF, and CSV
+# There were some differences between the different file types, 
+# but we only test the raster data type in this version of the script
+
+# Mean surface values for: 
+# temperature, salinity, sea ice thickness, chlorophyll a, nitrate, iron, and PAR present and future
+var_choices <- c("thetao", "so", "sithick", "chl", "no3", "dfe", "par")
+
+# Present baseline tests
+plyr::l_ply(var_choices, BO_v3_test, .parallel = TRUE)
+
+# RCP 4.5
+plyr::l_ply(var_choices, BO_v3_test, .parallel = TRUE, scenario = "ssp245")
+
+# RCP 8.5
+plyr::l_ply(var_choices, BO_v3_test, .parallel = TRUE, scenario = "ssp585")
 
